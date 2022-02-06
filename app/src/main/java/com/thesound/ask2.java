b58b64165c81e561f1970b29c23ec6fdb933562b
qkrwnstjd05 <jsp090505@gmail.com>
2022-02-06 17:00:42 +0900

    회원가입, 인증 페이지

---
+++ app/src/main/java/com/thesound/ask2.java
@@ -0,0 +1,14 @@
+package com.thesound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ask2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask2);
+    }
+}
---
+++ app/src/main/java/com/thesound/askX.java
@@ -0,0 +1,14 @@
+package com.thesound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class askX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_x);
+    }
+}
