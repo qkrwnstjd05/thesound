bbac2a51bd621a6c41c3be27a34ce2da18208450
yoonki <yoonkikim0108@gmail.com>
2022-02-13 15:14:56 +0900

    잠시

--- app/src/main/java/com/thesound/Main.java
+++ app/src/main/java/com/thesound/Main.java
@@ -2,12 +2,33 @@ package com.thesound;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
-
+    Button button5;
+    TextView textView8;
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView8=(TextView)findViewById(R.id.textView8);
+        button5=(Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i++;
+                if(i%2==0){
+                    textView8.setText("김윤기는 짝수만큼 바보다");
+                }
+                else{
+                    textView8.setText("김윤기는 홀수 보다 바보다");
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/thesound/login.java
+++ app/src/main/java/com/thesound/login.java
@@ -2,15 +2,26 @@ package com.thesound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
-
+    Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(login.this,Main.class);
+                startActivity(i);
+                finish();
+            }
+        });
         //login
     }
 }
