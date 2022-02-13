f09ef36530c7d0d0ab8398b4b599ca5ec4c58ee6
yoonki <yoonkikim0108@gmail.com>
2022-02-13 17:02:01 +0900

    검색 페이지 만드는 중 아직 임시 보관함 끝나지 않음

--- app/src/main/java/com/thesound/login.java
+++ app/src/main/java/com/thesound/login.java
@@ -1,27 +1,72 @@
 package com.thesound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
     Button button6;
+    Button button5;
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
+
+    // 이메일과 비밀번호
+    private EditText loginId;
+    private EditText loginPw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth=FirebaseAuth.getInstance();
         button6 = (Button) findViewById(R.id.button6);
+        button5 = (Button) findViewById(R.id.button5);
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(login.this,Main.class);
+                loginId=(EditText) findViewById(R.id.loginId);
+                loginPw=(EditText) findViewById(R.id.loginPw);
+                loginUser(loginId.getText().toString(),loginPw.getText().toString());
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(login.this,signup1.class);
                 startActivity(i);
-                finish();
             }
         });
         //login
+    }
+    // 로그인
+    private void loginUser(String email, String password)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Intent i = new Intent(login.this,Main.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/thesound/signup1.java
+++ app/src/main/java/com/thesound/signup1.java
@@ -1,14 +1,65 @@
 package com.thesound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup1 extends AppCompatActivity {
+
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
+
+    // 이메일과 비밀번호
+    private EditText textId;
+    private EditText textPw;
+
+    private String email = "";
+    private String password = "";
+
+    private Button button9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup1);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        button9 = (Button) findViewById(R.id.button9); //회원가입 버튼
+        textId=(EditText) findViewById(R.id.textId);
+        textPw=(EditText) findViewById(R.id.textPw);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser(textId.getText().toString(),textPw.getText().toString());
+            }
+        });
     }
+    // 회원가입
+    private void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공
+                            Toast.makeText(getApplicationContext(), "회원가입완료", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 회원가입 실패
+                            Toast.makeText(getApplicationContext(), "회원가입실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
