f37f2e14b6874154f952d202111c70744824b330
yoonki <yoonkikim0108@gmail.com>
2022-04-24 16:56:40 +0900

    올르골리 리액션 콰당탕탕

--- app/src/main/java/com/thesound/surch.java
+++ app/src/main/java/com/thesound/surch.java
@@ -1,8 +1,13 @@
 package com.thesound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class surch extends AppCompatActivity{
 
@@ -10,5 +15,36 @@ public class surch extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.surch);
+
+        BottomNavigationView bottomNavigation  = findViewById(R.id.bottomNav);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.go_to_search:
+                            Intent intent1=new Intent(getApplicationContext(),surch.class);
+                            startActivity(intent1);
+                            return true;
+                        case R.id.go_to_all_bulletin_board:
+                            Intent intent=new Intent(getApplicationContext(),petitionboard.class);
+                            startActivity(intent);
+                            return true;
+                        case R.id.go_to_home:
+                            Intent intent2=new Intent(getApplicationContext(),Main.class);
+                            startActivity(intent2);
+                            return true;
+                        case R.id.go_to_our_school_bulletin_board:
+                            Intent intent3=new Intent(getApplicationContext(),our_school_bullentin_board.class);
+                            startActivity(intent3);
+                            return true;
+                        case R.id.go_to_inbox:
+                            Intent intent4=new Intent(getApplicationContext(),storagebox.class);
+                            startActivity(intent4);
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
